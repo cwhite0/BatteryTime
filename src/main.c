abf884a9cb984c1e7cea64a1f3da8bc85c24c633
cwhite0 <chris@white-smith.net>
2016-01-01 21:33:50 -0500

    Reworked Alignment, made the date actual

--- src/main.c
+++ src/main.c
@@ -19,17 +19,23 @@ static void update_time() {
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
   static char date_buffer[16];
+  static char day_buffer[16];
+
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
                                           "%H:%M" : "%I:%M", tick_time);
 
   strftime(date_buffer, sizeof(date_buffer), 
                                           "%B %e", tick_time);
   
+  strftime(day_buffer, sizeof(day_buffer), 
+                                          "%A", tick_time);
+  
     
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
   text_layer_set_text(s_date_layer, date_buffer);
+  text_layer_set_text(s_dayOfWeek_layer, day_buffer);
   
   BatteryChargeState charge_state = battery_state_service_peek();
  if (charge_state.is_charging) {
@@ -56,13 +62,13 @@ static void main_window_load(Window *window) {
 
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w-20, 50));
 
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentRight);
   
   // Create Date Layer
   s_date_layer = text_layer_create(
