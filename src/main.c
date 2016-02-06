15d93ef00cc6b63c4b0d8bcf08d13527550c66e2
cwhite0 <chris@white-smith.net>
2016-02-05 22:38:38 -0500

    Tab Events cause steps and battery percent view

--- src/main.c
+++ src/main.c
@@ -5,11 +5,14 @@ static TextLayer *s_time_layer;
 static TextLayer *s_date_layer;
 static TextLayer *s_dayOfWeek_layer;
 static TextLayer *s_percent_layer;
+static TextLayer *s_step_layer;
+
 
 static GFont s_time_font;
 static GFont s_date_font;
 static GFont s_dayOfTheWeek_font;
 static GFont s_percent_font;
+static GFont s_step_font;
 
 
 static void update_time() {
@@ -22,6 +25,7 @@ static void update_time() {
   static char date_buffer[16];
   static char day_buffer[16];
   static char percent_buffer[8];
+  static char step_buffer[16];
 
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
                                           "%H:%M" : "%I:%M", tick_time);
@@ -41,23 +45,37 @@ static void update_time() {
   BatteryChargeState charge_state = battery_state_service_peek();
   snprintf (percent_buffer, sizeof(percent_buffer), "%d%%", charge_state.charge_percent);
   text_layer_set_text(s_percent_layer, percent_buffer);   
+  
+  HealthMetric metric = HealthMetricStepCount;
+  time_t start = time_start_of_today();
+  time_t end = time(NULL);
+
+  // Check the metric has data available for today
+  HealthServiceAccessibilityMask mask = health_service_metric_accessible(metric, 
+    start, end);
+
+  if(mask & HealthServiceAccessibilityMaskAvailable) {
+    // Data is available!
+     snprintf (step_buffer, sizeof(step_buffer), "%d", (int)health_service_sum_today(metric));
+    text_layer_set_text(s_step_layer, step_buffer);   
+
+  } 
  
  if (charge_state.is_charging) {
    text_layer_set_text_color(s_time_layer, GColorVividCerulean);
-   text_layer_set_text_color(s_percent_layer, GColorVividCerulean); 
  } else if (charge_state.charge_percent < 11) {
   text_layer_set_text_color(s_time_layer, GColorMelon);
-   text_layer_set_text_color(s_percent_layer, GColorMelon); 
  } else if (charge_state.charge_percent < 31) {
     text_layer_set_text_color(s_time_layer, GColorRajah);
-    text_layer_set_text_color(s_percent_layer, GColorRajah); 
  } else if (charge_state.charge_percent < 101) {
     text_layer_set_text_color(s_time_layer, GColorWhite);
-    text_layer_set_text_color(s_percent_layer, GColorBlack); 
  } else {
     text_layer_set_text_color(s_time_layer, GColorLavenderIndigo);
-   text_layer_set_text_color(s_percent_layer, GColorLavenderIndigo); 
+
  }
+ text_layer_set_text_color(s_step_layer, GColorClear);
+ text_layer_set_text_color(s_percent_layer, GColorClear); 
+  
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -104,8 +122,17 @@ static void main_window_load(Window *window) {
   // Style te text
   text_layer_set_background_color(s_percent_layer, GColorClear);
   text_layer_set_text_color(s_percent_layer, GColorPictonBlue);
-  text_layer_set_text_alignment(s_percent_layer, GTextAlignmentRight);
+  text_layer_set_text_alignment(s_percent_layer, GTextAlignmentLeft);
   text_layer_set_text(s_percent_layer, "0%");
+  
+  s_step_layer = text_layer_create(
+    GRect(0, PBL_IF_ROUND_ELSE(0, 0), bounds.size.w - 20, 30));
+
+  // STEPs
+  text_layer_set_background_color(s_step_layer, GColorClear);
+  text_layer_set_text_color(s_step_layer, GColorWhite);
+  text_layer_set_text_alignment(s_step_layer, GTextAlignmentLeft);
+  text_layer_set_text(s_step_layer, "0");
 
   // Create GFont
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_GLACIALINDIFFERENCE_REGULAR_48));
@@ -113,6 +140,8 @@ static void main_window_load(Window *window) {
   s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_GLACIALINDIFFERENCE_REGULAR_24));
   s_dayOfTheWeek_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_GLACIALINDIFFERENCE_REGULAR_18));
   s_percent_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_GLACIALINDIFFERENCE_REGULAR_12));
+  s_step_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_GLACIALINDIFFERENCE_REGULAR_18));
+
 
 
   // Apply to TextLayer
@@ -121,7 +150,8 @@ static void main_window_load(Window *window) {
   // Apply to DateLayer
   text_layer_set_font(s_date_layer, s_date_font);
   text_layer_set_font(s_percent_layer, s_percent_font);
-  
+  text_layer_set_font(s_step_layer, s_step_font);
+
   // Apply DateOfTheWeek
   text_layer_set_font(s_dayOfWeek_layer, s_dayOfTheWeek_font);
 
@@ -133,6 +163,8 @@ static void main_window_load(Window *window) {
     // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_dayOfWeek_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_percent_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_step_layer));
+
   
 }
 
@@ -142,6 +174,8 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_date_layer);
   text_layer_destroy(s_dayOfWeek_layer);
   text_layer_destroy(s_percent_layer);
+  text_layer_destroy(s_step_layer);
+
 
  // Unload GFont
   fonts_unload_custom_font(s_time_font);
@@ -151,6 +185,10 @@ static void main_window_unload(Window *window) {
 
 }
 
+static void tap_handler(AccelAxisType axis, int32_t direction) {
+ text_layer_set_text_color(s_step_layer, GColorWhite);
+ text_layer_set_text_color(s_percent_layer, GColorWhite); 
+}
 
 static void init() {
   // Create main Window element and assign to pointer
@@ -173,11 +211,16 @@ static void init() {
 
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  // Tap handler
+  accel_tap_service_subscribe(tap_handler);
+
 }
 
 static void deinit() {
   // Destroy Window
   window_destroy(s_main_window);
+ accel_tap_service_unsubscribe();
+
 }
 
 int main(void) {
